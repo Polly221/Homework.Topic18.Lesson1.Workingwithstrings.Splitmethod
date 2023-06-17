fa37ae6f2289ba6d0b50737a42a552e6a0e2c743
Xtrat3 <Xtrat3@XTRAT3>
2023-06-17 12:57:55 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] forbiddenWords = { "Ты ", "все", "сможешь" };
+string sentence = "";
+while (true)
+{
+    Console.Write("Введите слово (для завершения введите 'конец'): ");
+    string word = Console.ReadLine();
+for (int i = 0; i < forbiddenWords.Length; i++)
+    {
+        if (word == forbiddenWords[i])
+        {
+            word = "(скрыто)";
+            break;
+        }
+    }
+    if (word == "конец")
+    {
+        break;
+    }
+    sentence += word + " ";
+}
+Console.WriteLine("Сформированное предложение: " + sentence);
